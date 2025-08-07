51fb539da510f554521c7dbe7a96aa61e188eafc
pengyiqiang <pengyiqiang@xiaomi.com>
2025-08-07 17:42:41 +0800

    fix: re-enable scissor

--- VGLite/Series/gc355/0x0_1216/vg_lite_options.h
+++ VGLite/Series/gc355/0x0_1216/vg_lite_options.h
@@ -34,7 +34,7 @@
 #define ECOID           0x0
 
 #define gcFEATURE_VG_IM_INDEX_FORMAT            0
-#define gcFEATURE_VG_SCISSOR                    0
+#define gcFEATURE_VG_SCISSOR                    1
 #define gcFEATURE_VG_BORDER_CULLING             0
 #define gcFEATURE_VG_RGBA2_FORMAT               0
 #define gcFEATURE_VG_QUALITY_8X                 0
